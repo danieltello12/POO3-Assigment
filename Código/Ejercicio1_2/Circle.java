38097ace93daf9ccc1fb9a06a10cebc0acffbc2d
UAH <pediludium05@gmail.com>
2024-02-07 09:38:18 +0100

    Inicio

--- Código/Ejercicio1_2/Circle.java
+++ Código/Ejercicio1_2/Circle.java
@@ -1,4 +1,32 @@
 package Ejercicio1_2;
 
 public class Circle {
+    double radius=1.0;
+    public void circle(){
+    }
+    public void circle(Double radio){
+        radius=radio;
+
+    }
+    public double getRadio(){
+        return radius;
+    }
+    public void setRadio(Double radio){
+        radius=radio;
+
+    }
+    public double getArea(){
+        double x=0.2;
+        return x;
+    }
+    public double getCircunferencia(){
+        double x=0.2;
+        return x;
+
+    }
+    public String toString(){
+        String x= "HOla";
+        return x;
+
+    }
 }
